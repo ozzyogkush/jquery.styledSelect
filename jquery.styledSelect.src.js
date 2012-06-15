4f3bc7c020a90cf23aa0efc2c3bb6b878e5dc5a5
Derek Rosenzweig <derek.rosenzweig@gmail.com>
2012-06-15 10:50:56 -0400

    Changes in v1.1:
    The original select element no longer needs the 'data-styled-select-id' property.
    
    Instead, pass the 'styled_select_id' option along when initializing the widget.
    
    The element that displays the selected item's label is now a div that fits more fully.
    
    The arrow's left border now will be styled the same as the replacement widget's border style.
    
    This keeps it in line with the method that jquery.styledFileInput uses, for consistency.

--- jquery.styledSelect.src.js
+++ jquery.styledSelect.src.js
@@ -16,17 +16,19 @@
  * Requires jQuery.js 1.7.0 or higher to function.
  *
  * Usage:
- * 	Create a regular <select> widget in HTML and give it the 'data-styled-select-id'
- * 	property. Override any CSS styles needed for your theme. On page DOM load, simply
- * 	call the styledSelectBox() function with or without any special options. 
+ * 	Create a regular <select> widget in HTML. Override any CSS styles needed for
+ * 	your theme. On page DOM load, simply call the styledSelectBox() function with
+ * 	or without any special options. Required options:
+ * 		styled_select_id		the ID of the new element that will be created
+ * 		image_base				location of directory where images are located
  * 
  * @example		See example.html
  * @class		StyledSelectBox
  * @name		StyledSelectBox
- * @version		1.0
+ * @version		1.1
  * @author		Derek Rosenzweig <derek.rosenzweig@gmail.com, drosenzweig@riccagroup.com>
  */
-(function($){
+(function($) {
 	
 	/**
      * Constructor. Creates the new 'styled_select' <div> and its children, sets the indicator
@@ -57,8 +59,10 @@
 		 * @type		Object
 		 * @memberOf	StyledSelectBox
 		 * @since		1.0
+		 * @updated		1.1
 		 */
 		var default_options = {
+			styled_select_id : null,				// ID of the new element that'll be created. Default null. Required.
 			image_base : null,						// Location of directory where images are located. Default null. Required.
 			classes : [],							// A set of additional class names that will get applied to the replacement 'styled_select' <div>. Default []. Optional.
 			widget_height : null					// The height of the widget in pixels. Default null. Optional.
@@ -89,16 +93,16 @@
 		var replacement_container_div = null;
 		
 		/**
-		 * The 'styled_select_option_display' <span> element that contains the text
+		 * The 'styled_select_option_display' <div> element that contains the text
 		 * of the currently selected <select> widget <option> value.
 		 *
 		 * @access		public
-		 * @type		HTMLElement <span>
+		 * @type		HTMLElement <div>
 		 * @memberOf	StyledSelectBox
 		 * @since		1.0
 		 * @default		null
 		 */
-		var selected_option_span = null;
+		var selected_option_div = null;
 		
 		/**
 		 * The 'styled_select_arrow' <span> element that contains the indicator
@@ -128,11 +132,12 @@
 		 * @access		public
 		 * @memberOf	StyledSelectBox
 		 * @since		1.0
+		 * @updated		1.1
 		 */
 		this.initStyledSelectBox = function() {
-			// First check for valid 'data-styled-select-id' property.
-			if (linked_select_box.attr('data-styled-select-id') === undefined) {
-				alert('StyledSelectBox widget: no "data-styled-select-id" property found on the original Select widget.');
+			// First check for valid 'styled_select_id' option.
+			if (options.styled_select_id == null) {
+				alert('StyledSelectBox widget: no "styled_select_id" option was passed.');
 				return;
 			}
 			// Next check for required options.
@@ -142,22 +147,33 @@
 			}
 			
 			// Create the replacement widget elements.
-			replacement_container_div = $("<div class='styled_select' id='"+linked_select_box.attr('data-styled-select-id')+"'></div>");
-			selected_option_span = $("<span class='styled_select_option_display'></span>");
-			arrow_span = $("<span class='styled_select_arrow'>&nbsp;</span>").css({backgroundImage:'url('+options.image_base+'/small-arrow.png'+')'});
+			replacement_container_div = $("<div class='styled_select' id='"+options.styled_select_id+"'></div>");
+			selected_option_div = $("<div class='styled_select_option_display'></div");
+			var arrow_left_border = replacement_container_div.css('border-left-width') + ' ' + replacement_container_div.css('border-left-style') + ' ' + replacement_container_div.css('border-left-color');
+			arrow_span = $("<span class='styled_select_arrow'>&nbsp;</span>").css({backgroundImage:'url('+options.image_base+'/small-arrow.png'+')',
+																				   borderLeft: arrow_left_border});
+			replacement_container_div.append(selected_option_div).append(arrow_span);
+			linked_select_box.addClass('original_select_now_styled');
 			
-			replacement_container_div.append(selected_option_span).append(arrow_span);
+			// Set the calculated widths
+			replacement_container_div.css({width:linked_select_box.outerWidth()+'px'});;
+			selected_option_div.css({width:(parseInt(replacement_container_div.innerWidth()-arrow_span.innerWidth()))+'px'});
 			
+			// Set the heights
 			if (options.widget_height != null) {
 				linked_select_box.css({height:options.widget_height + "px",
 									   lineHeight:(parseInt(options.widget_height)-2) + "px"});
 				replacement_container_div.css({height:options.widget_height + "px"});
+				selected_option_div.css({height:options.widget_height + "px"});
 				if ($.browser.msie && parseInt($.browser.version) <= 8) {
 					replacement_container_div.css({lineHeight:options.widget_height + "px"});
 				}
 				else {
 					replacement_container_div.css({lineHeight:(parseInt(options.widget_height)-2) + "px"});
 				}
+			}
+			else {
+				selected_option_div.css({height:linked_select_box.height() + "px"});
 			}
 			
 			// Add optional classes.
@@ -187,13 +203,14 @@
 		 * @public
 		 * @memberOf	StyledSelectBox
 		 * @since		1.0
+		 * @updated		1.1
 		 *
 		 * @param		change_event			jQuery.Event				jQuery 'change' Event
 		 */
 		this.setCurrentSelectedText = function(change_event) {
 			var select_box = linked_select_box.get(0);
 			var opts = linked_select_box.find('option');//select_box.children;
-			replacement_container_div.find('span.styled_select_option_display').html(opts[select_box.selectedIndex].text);
+			replacement_container_div.find('div.styled_select_option_display').html(opts[select_box.selectedIndex].text);
 			linked_select_box.blur();
 		}
 		
